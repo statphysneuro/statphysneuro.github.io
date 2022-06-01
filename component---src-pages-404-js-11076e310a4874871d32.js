ff2d068e3980a7c284c7d1c5cafc6f2c52bc22d1
Sebastian Goldt <goldt.sebastian@gmail.com>
2022-06-01 19:09:45 +0200

    Deleted everything

--- component---src-pages-404-js-11076e310a4874871d32.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_serif_theme=self.webpackChunkgatsby_serif_theme||[]).push([[883],{9616:function(e,t,n){n.r(t);var u=n(7434),r=n(7294),s=n(4737),a=function(e){function t(){return e.apply(this,arguments)||this}return(0,u.Z)(t,e),t.prototype.render=function(){return r.createElement(s.Z,null,r.createElement("h1",null,"Not Found"),r.createElement("p",null,"You just hit a page that doesn't exist..."))},t}(r.Component);t.default=a}}]);
-//# sourceMappingURL=component---src-pages-404-js-11076e310a4874871d32.js.map
